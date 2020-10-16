339e765529b6f008673ee5a8239544aafb8053a4
Kenneth Ekandem <kennyekandem@gmail.com>
2020-10-16 12:24:42 +0100

    first push

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index() {
+
+        $users = User::all();
+
+        return response()->json([
+            'status_code' => 200,
+            'data' => $users,
+        ]);
+    }
+}
